aba73e166d7a0778af9e3df901ba328071363846
aaalex <95416488+aaalexm@users.noreply.github.com>
2022-04-05 21:22:04 +0200

    Create installation_scrispr.R

---
+++ installation_scrispr.R
@@ -0,0 +1,34 @@
+#!/usr/bin/env R
+## Installation script for ScrispR
+
+if (!("optparse" %in% installed.packages())) { 
+  install.packages("optparse");
+}
+
+if (!("ggplot2" %in% installed.packages())) { 
+  install.packages("ggplot2");
+}
+
+if (!("tidyr" %in% installed.packages())) { 
+  install.packages("tidyr");
+}
+
+if (!("reshape2" %in% installed.packages())) { 
+  install.packages("reshape2");
+}
+
+if (!("dplyr" %in% installed.packages())) { 
+  install.packages("dplyr");
+}
+
+if (!("stringr" %in% installed.packages())) { 
+  install.packages("stringr");
+}
+
+if (!("tidyverse" %in% installed.packages())) { 
+  install.packages("tidyverse");
+}
+
+if (!("tidyverse" %in% installed.packages())) { 
+  install.packages("tidyverse");
+}
