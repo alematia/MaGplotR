e4d7f4eb2c5fd0c2163f2a24beba8acb48888978
alematia <95416488+alematia@users.noreply.github.com>
2022-08-15 16:17:40 -0700

    Update MaGplotR.R
    Add colourblind option if chosen by user

--- MaGplotR.R
+++ MaGplotR.R
@@ -99,6 +99,14 @@ parser <- add_option(parser,
 )
 
 
+parser <- add_option(parser, 
+                     opt_str = c("-b", "--blind"), 
+                     type = "character",
+                     dest = 'colour.blind',
+                     help="A string: 'y' or 'n'. 'n' as default."
+)
+
+
 opt = parse_args(parser)
 
 
@@ -147,6 +155,13 @@ selection = opt$selection
 
 # Select Gene Ontology option
 if(!is.null(opt$gene.ontology)){gene_ontology <- opt$gene.ontology}
+
+
+if(!is.null(opt$colour.blind)){
+  col_blind <- opt$colour.blind
+} else {
+  col_blind <- "n"
+}
 
 
 # Read MaGeCK files
@@ -299,40 +314,40 @@ gene_analysis <- function(x = input_files_txt, y = control_file){
     ## Set limits to LFCs. All above 10 and below -5 is considered as max and min respectively.
     melted_LFC_pos$value[melted_LFC_pos$value > 10] <- 10  # All LFCs above 10 are now 10.
     melted_LFC_pos$value[melted_LFC_pos$value < -5] <- -5  # All LFCs below -5 are now -5.
-    heatmap_mg_pos <- ggplot(melted_LFC_pos, aes(x=variable, y=reorder(id, -RankMeans), fill=value))+
-      geom_tile(colour="black", size=.2)+
-      ggtitle("Gene LFC")+
-      geom_text(size=1.5, aes(label = round(melted_mg_pos$value, 1)))+
-      theme(panel.background = element_blank(),
-            legend.position = "left",
-            axis.text.x = element_text(angle = 45, hjust = 1),
-            axis.title.x = element_blank(),
-            axis.title.y = element_blank())+
-      scale_fill_distiller(palette = "RdYlGn", direction = 1, name="LFC",
-                           limits = c(-5, 10), labels=c("< -5", "0", "5", "> 10"),
-                           values = c(0, 0.25, 0.45,1))
+    
+    if (col_blind == "y"){
+      heatmap_mg_pos <- ggplot(melted_LFC_pos, aes(x=variable, y=reorder(id, -RankMeans), fill=value))+
+        geom_tile(colour="black", size=.2)+
+        ggtitle("Gene LFC")+
+        geom_text(size=1.5, aes(label = round(melted_mg_pos$value, 1)))+
+        theme(panel.background = element_blank(),
+              legend.position = "left",
+              axis.text.x = element_text(angle = 45, hjust = 1),
+              axis.title.x = element_blank(),
+              axis.title.y = element_blank())+
+        scale_fill_distiller(palette = "RdYlBu", direction = 1, name="LFC",
+                             limits = c(-5, 10), labels=c("< -5", "0", "5", "> 10"),
+                             values = c(0, 0.25, 0.45,1))
+    } else {
+      heatmap_mg_pos <- ggplot(melted_LFC_pos, aes(x=variable, y=reorder(id, -RankMeans), fill=value))+
+        geom_tile(colour="black", size=.2)+
+        ggtitle("Gene LFC")+
+        geom_text(size=1.5, aes(label = round(melted_mg_pos$value, 1)))+
+        theme(panel.background = element_blank(),
+              legend.position = "left",
+              axis.text.x = element_text(angle = 45, hjust = 1),
+              axis.title.x = element_blank(),
+              axis.title.y = element_blank())+
+        scale_fill_distiller(palette = "RdYlGn", direction = 1, name="LFC",
+                             limits = c(-5, 10), labels=c("< -5", "0", "5", "> 10"),
+                             values = c(0, 0.25, 0.45,1))
+    }
     #scale_fill_distiller(palette = "YlGn", direction = 1, name="LFC",
     #                    limits = c(-5, 10), labels=c("< -5", "0", "5", "> 10"))
     #scale_fill_distiller(palette = 4, direction = 1, name="LFC",
     #                     limits = c(min(melted_LFC$value), max(melted_LFC$value)))
     #scale_fill_distiller(palette = 5, limits = c(min(melted_LFC$value)-2, max(melted_LFC$value)+2), direction = 1)
-    
-    ### Previous heatmap
-    ## Melt df to tidy large format
-    #melted_mg_pos <- melt(top_mg_pos,id.vars = c("id", "RankMeans", "RankSD"))
-    #heatmap_mg_pos <- ggplot(melted_mg_pos, aes(x=variable, y=reorder(id, -RankMeans), fill=value))+
-    #  geom_tile(colour="white", size=.2)+
-    #  ggtitle("Gene position in positive rank")+
-    #  theme(panel.background = element_blank(),
-    #        legend.position = "left",
-    #        axis.text.x = element_text(angle = 45, hjust = 1),
-    #        axis.title.x = element_blank(),
-    #        axis.title.y = element_blank())+
-    #  scale_fill_distiller(palette = "RdYlGn", direction = 1, trans= "reverse", 
-    #                       name="", labels = c("Highest\nposition\nin rank", "Lowest\nposition\nin rank"),
-    #                       breaks=seq(1, nrow(sorted_whole_mg_pos), by=nrow(sorted_whole_mg_pos)-1))  # Legend scale from highest to lowest position in rank
-    
-  } else {
+   } else {
     ## Preparation for neg
     sub_mg_rank_files_neg <- id_rank_maker_neg(input_files_txt)
     merged_mg_neg <- sub_mg_rank_files_neg %>% reduce(inner_join, by = "id")  # Merge all dfs by id
@@ -352,6 +367,7 @@ gene_analysis <- function(x = input_files_txt, y = control_file){
     ## Set limits to LFCs. All above 10 and below -5 is considered as max and min respectively.
     melted_LFC_neg$value[melted_LFC_neg$value > 10] <- 10  # All LFCs above 10 are now 10.
     melted_LFC_neg$value[melted_LFC_neg$value < -5] <- -5  # All LFCs below -5 are now -5.
+    if (col_blind == "y"){
     heatmap_mg_neg <- ggplot(melted_LFC_neg, aes(x=variable, y=reorder(id, -RankMeans), fill=value))+
       geom_tile(colour="black", size=.2)+
       ggtitle("Gene LFC")+
@@ -361,38 +377,23 @@ gene_analysis <- function(x = input_files_txt, y = control_file){
             axis.text.x = element_text(angle = 45, hjust = 1),
             axis.title.x = element_blank(),
             axis.title.y = element_blank())+
-      scale_fill_distiller(palette = "RdYlGn", direction = 1, name="LFC",
+      scale_fill_distiller(palette = "RdYlBu", direction = 1, name="LFC",
                            limits = c(-5, 10), labels=c("< -5", "0", "5", "> 10"),
                            values = c(0, 0.25, 0.45,1))
-    #scale_fill_distiller(palette = "YlGn", direction = 1, name="LFC",
-    #                    limits = c(-5, 10), labels=c("< -5", "0", "5", "> 10"))
-    #scale_fill_distiller(palette = 4, direction = 1, name="LFC",
-    #                     limits = c(min(melted_LFC$value), max(melted_LFC$value)))
-    #scale_fill_distiller(palette = 5, limits = c(min(melted_LFC$value)-2, max(melted_LFC$value)+2), direction = 1)
-    
-    ## Previous heatmap
-    #sub_mg_rank_files_neg <- id_rank_maker_neg(input_files_txt)
-    #merged_mg_neg <- sub_mg_rank_files_neg %>% reduce(inner_join, by = "id")
-    #rank_data_mg_neg <- merged_mg_neg %>% 
-    #  select(matches(" "))
-    #merged_mg_neg_x <- merged_mg_neg  # Replicate df to add more cols
-    #merged_mg_neg_x$RankMeans <- rowMeans(rank_data_mg_neg)  # Add means
-    #merged_mg_neg_x$RankSD <- rowSds(as.matrix(rank_data_mg_neg)) # Add Variance of the rank scores
-    #sorted_whole_mg_neg <- merged_mg_neg_x[order(merged_mg_neg_x$RankMeans),]
-    #top_mg_neg <- head(sorted_whole_mg_neg, as.numeric(top_cutoff))
-    #melted_mg_neg <- melt(top_mg_neg,id.vars = c("id", "RankMeans", "RankSD"))
-    #heatmap_mg_neg <- ggplot(melted_mg_neg, aes(x=variable, y=reorder(id, -RankMeans), fill=value))+
-    #  geom_tile(colour="white", size=.2)+
-    #  ggtitle("Gene position in negative rank")+
-    #  theme(panel.background = element_blank(),
-    #        legend.position = "left",
-    #        axis.text.x = element_text(angle = 45, hjust = 1),
-    #        axis.title.x = element_blank(),
-    #        axis.title.y = element_blank())+
-    #  scale_fill_distiller(palette = "RdYlGn", direction = 1, trans= "reverse", 
-    #                       name="", labels = c("Highest\nposition\nin rank", "Lowest\nposition\nin rank"),
-    #                       breaks=seq(min(melted_mg_neg$value), max(melted_mg_neg$value),
-    #                                  by=(max(melted_mg_neg$value)-min(melted_mg_neg$value))))  # Legend scale from highest to lowest position in rank
+    } else {
+      heatmap_mg_neg <- ggplot(melted_LFC_neg, aes(x=variable, y=reorder(id, -RankMeans), fill=value))+
+        geom_tile(colour="black", size=.2)+
+        ggtitle("Gene LFC")+
+        geom_text(size=1.5, aes(label = round(melted_mg_neg$value, 1)))+
+        theme(panel.background = element_blank(),
+              legend.position = "left",
+              axis.text.x = element_text(angle = 45, hjust = 1),
+              axis.title.x = element_blank(),
+              axis.title.y = element_blank())+
+        scale_fill_distiller(palette = "RdYlGn", direction = 1, name="LFC",
+                             limits = c(-5, 10), labels=c("< -5", "0", "5", "> 10"),
+                             values = c(0, 0.25, 0.45,1))
+    }
   }
   
   # Save pos or neg
@@ -469,7 +470,7 @@ gene_analysis <- function(x = input_files_txt, y = control_file){
   suppressPackageStartupMessages(library(org.Hs.eg.db))  # Library is loaded here to avoid overlapping function errors
     
   if (selection == "pos"){
-    #Reactome pos
+    # Reactome pos
     perc_num_pos <- round(nrow(sorted_whole_mg_pos)*0.01)  # Obtain the top 1 %.
     pre_go_pos <- head(sorted_whole_mg_pos, perc_num_pos)
     go_genes_pos <- pre_go_pos$id
@@ -492,7 +493,7 @@ gene_analysis <- function(x = input_files_txt, y = control_file){
       scale_color_distiller(palette = "RdPu", values = c(0, 0.25, 1))
     #OrRd #RdPu #YlGn
   } else {
-    #Reactome neg
+    # Reactome neg
     perc_num_neg <- round(nrow(sorted_whole_mg_neg)*0.01)  # Obtain the top 1 %.
     pre_go_neg <- head(sorted_whole_mg_neg, perc_num_neg)
     go_genes_neg <- pre_go_neg$id
@@ -515,7 +516,7 @@ gene_analysis <- function(x = input_files_txt, y = control_file){
       scale_color_distiller(palette = "RdPu", values = c(0, 0.25, 1))
   }
   
-  #Save pos or neg
+  # Save pos or neg
   if(selection == "pos"){
     suppressMessages(ggsave(width = 10, path = output.directory, filename = paste0("ReactomePA_pos.", plot.format), plot = pathways_pos_plot, device = plot.format))
     write.csv(pathways_pos@result, paste0(output.directory, "/ReactomePA_pos.csv"), row.names = FALSE)
@@ -538,7 +539,7 @@ gene_analysis <- function(x = input_files_txt, y = control_file){
   print(str_glue("- Reactome Pathway Analysis completed."))
   
   #### CLUSTERING by clusterProfiler. Top 1 %
-  #Pos or neg
+  # Pos or neg
   if (selection == "pos"){
     num <- 1
     for (i in input_files_txt){
@@ -575,7 +576,7 @@ gene_analysis <- function(x = input_files_txt, y = control_file){
       names(sym_ids_vector)[[num]] <- num
       num <- num + 1
     }
-    #Create the object
+    # Create the object
     cluster_pos <- compareCluster(geneClusters = sym_ids_vector, fun = "enrichPathway")
     if (!is.null(cluster_pos)){
       cluster_pos@compareClusterResult$Description <- gsub("Homo sapiens\r: ", "", as.character(cluster_pos@compareClusterResult$Description))
@@ -589,7 +590,7 @@ gene_analysis <- function(x = input_files_txt, y = control_file){
     }
     detach("package:org.Hs.eg.db", unload=TRUE)
   } else {
-    #neg
+    # neg
     num <- 1
     for (i in input_files_txt){
       input_files_txt[[num]] <- input_files_txt[[num]][order(input_files_txt[[num]]$neg.rank),]
@@ -626,7 +627,7 @@ gene_analysis <- function(x = input_files_txt, y = control_file){
       names(sym_ids_vector)[[num]] <- num
       num <- num + 1
     }
-    #Create the object
+    # Create the object
     cluster_neg <- compareCluster(geneClusters = sym_ids_vector, fun = "enrichPathway")
     if (!is.null(cluster_neg)){
       cluster_neg@compareClusterResult$Description <- gsub("Homo sapiens\r: ", "", as.character(cluster_neg@compareClusterResult$Description))
@@ -703,7 +704,7 @@ sgrna_analysis <- function(sginput_files_txt){
   # Execute functions
   sgsub_input_files <- sgrna_shortener(sginput_files_txt)
   sgsub_input_files_coln <- col_appender(sgsub_input_files)
-  sgbound <- bind_rows(as.vector(sgsub_input_files_coln)) # Binds all sgRNA sub dfs
+  sgbound <- bind_rows(as.vector(sgsub_input_files_coln))  # Binds all sgRNA sub dfs
   
   # Create boxplot for sgRNAs' LFCs
   sgboxplot <- ggplot(sgbound, aes(x = factor(exp_number), y = LFC))+
